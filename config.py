1deaa90d8455d62ca9e3b5782e8c5f7c4d8d7cd8
JONATHAN-DEV-01 <jonathanads2006@gmail.com>
2025-05-24 18:17:04 -0300

    alteração de porta

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     HOST = '0.0.0.0'
-    PORT = 5001 
+    PORT = 5002
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///reservas.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
